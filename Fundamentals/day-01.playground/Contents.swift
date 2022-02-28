72f433356faa3f3f1ab46689e94f6908f0200328
Luiz Hartmann <luiz-hartmann@outlook.com>
2022-02-27 20:58:01 -0400

    First steps in Swift

---
+++ Fundamentals/day-01.playground/Contents.swift
@@ -0,0 +1,33 @@
+import UIKit
+
+// Variables
+var greeting = "Hello, playground"
+greeting = "Goodbye"
+
+// String and integers
+var age = 24
+var hours = 1_000_000
+
+// Multi-line strings
+var movie = """
+This goes
+over multiple
+lines
+"""
+
+// Doubles and booleans
+var pi = 3.171
+var awesome = true
+
+// String interpolation
+var score = 95
+var result = "Your score was \(score)"
+
+// Constants
+let taylor = "swift"
+
+// Type annotations
+let album: String = "Reputation"
+let year: Int = 1989
+let height: Double = 1.78
+let taylorRocks: Bool = true
