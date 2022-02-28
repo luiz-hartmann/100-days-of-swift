8f6d96023b16ea413ed5fbcc838a3442a694ed6a
Luiz Hartmann <luiz-hartmann@outlook.com>
2022-02-28 03:20:42 -0400

    Project 03

---
+++ Projects/03 - Social Media/Social Media/DetailViewController.swift
@@ -0,0 +1,52 @@
+//
+//  DetailViewController.swift
+//  Social Media
+//
+//  Created by Luiz Hartmann on 19/02/22.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+    
+    @IBOutlet var imageView: UIImageView!
+    
+    var selectedImage: String?
+   
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        navigationItem.largeTitleDisplayMode = .never
+
+        // title = selectedImage
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .action, target: self, action: #selector(shareTapped))
+        if let imageToLoad = selectedImage {
+            imageView.image = UIImage(named: imageToLoad)
+        }
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.hidesBarsOnTap = true
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        navigationController?.hidesBarsOnTap = false
+    }
+    
+    @objc func shareTapped() {
+        guard let image = imageView.image?.jpegData(compressionQuality: 0.8) else {
+            print("No image found")
+            return
+        }
+
+        if let imageName = selectedImage {
+            imageView.image = UIImage(named: imageName)
+            
+            let viewController = UIActivityViewController(activityItems: [image, imageName], applicationActivities: nil)
+            viewController.popoverPresentationController?.barButtonItem = navigationItem.rightBarButtonItem
+            present(viewController, animated: true)
+        }
+    }
+}
