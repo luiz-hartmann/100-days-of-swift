2bc0135576839f215a7c298c0a51c7e3654570c1
Luiz Hartmann <luiz-hartmann@outlook.com>
2022-03-06 22:18:00 -0400

    Milestone: Projects 1-3

---
+++ Projects/Milestone/Country Flags/Flags.swift
@@ -0,0 +1,16 @@
+//
+//  Flags.swift
+//  Country Flags
+//
+//  Created by Luiz Hartmann on 04/03/22.
+//
+
+import Foundation
+
+struct Country: Codable {
+    let code: String
+    let name: String
+}
+
+
+
