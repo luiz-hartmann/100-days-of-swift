0f962ff64d4d35f3c1026790d79af7cf60804946
Luiz Hartmann <luiz-hartmann@outlook.com>
2022-03-06 22:20:52 -0400

    Milestone: Projects 1-3

---
+++ Projects/Milestone/Projects 1-3/Country Flags/FlagCell.swift
@@ -0,0 +1,14 @@
+//
+//  FlagCell.swift
+//  Country Flags
+//
+//  Created by Luiz Hartmann on 06/03/22.
+//
+
+import UIKit
+
+class FlagCell: UITableViewCell {
+
+    @IBOutlet var flagImageView: UIImageView!
+    @IBOutlet var flagTextLabel: UILabel!
+}
