d70aa684ebc01afeac5d59cbad1d31e6c5794e08
Luiz Hartmann <luiz-hartmann@outlook.com>
2022-03-14 22:13:50 -0400

    Word Scramble

---
+++ Projects/Project-05/Word Scramble/ViewController.swift
@@ -0,0 +1,64 @@
+//
+//  ViewController.swift
+//  Word Scramble
+//
+//  Created by Luiz Hartmann on 13/03/22.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+    
+    var allWords = [String]()
+    var usedWords = [String]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(promptForAnswer))
+        
+        if let startWordsURL = Bundle.main.url(forResource: "start", withExtension: "txt") {
+            if let startWords = try? String(contentsOf: startWordsURL) {
+                allWords = startWords.components(separatedBy: "\n")
+            }
+        }
+        
+        if allWords.isEmpty {
+            allWords = ["silkworm"]
+        }
+        
+        func startGame() {
+            title = allWords.randomElement()
+            usedWords.removeAll(keepingCapacity: true)
+            tableView.reloadData()
+        }
+        startGame()
+    }
+
+    func submit(_ answer: String) {
+        
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return usedWords.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Word", for: indexPath)
+        cell.textLabel?.text = usedWords[indexPath.row]
+        return cell
+    }
+    
+    @objc func promptForAnswer() {
+        let alertController = UIAlertController(title: "Enter answer", message: nil, preferredStyle: .alert)
+        alertController.addTextField()
+        
+        let submitAction = UIAlertAction(title: "Submit", style: .default) { [weak self, weak alertController] action in
+            guard let answer = alertController?.textFields?[0].text else { return }
+            self?.submit(answer)
+        }
+        
+        alertController.addAction(submitAction)
+        present(alertController, animated: true)
+    }
+}
