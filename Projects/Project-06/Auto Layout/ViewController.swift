6df27878dd4916490e1609bca93a12ef858703d6
Luiz Hartmann <luiz-hartmann@outlook.com>
2022-03-20 18:06:21 -0400

    Auto Layout

---
+++ Projects/Project-06/Auto Layout/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Auto Layout
+//
+//  Created by Luiz Hartmann on 20/03/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
